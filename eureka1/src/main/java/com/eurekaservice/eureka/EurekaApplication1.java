c2bc0061d9ef6d447c435100304916656bdb6779
soethandar90 <soethandar18@gmail.com>
2023-04-15 18:00:02 -0500

    Modify Eureka 1

---
+++ eureka1/src/main/java/com/eurekaservice/eureka/EurekaApplication1.java
@@ -0,0 +1,15 @@
+package com.eurekaservice.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication1.class, args);
+	}
+
+}
